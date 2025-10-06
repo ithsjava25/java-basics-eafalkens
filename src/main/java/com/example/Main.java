62670570658e9d532b11f9defca2b26ad363e36d
Erika Falk Svensen <alicia-fs@outlook.com>
2025-10-06 14:16:51 +0200

    andra commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,17 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import com.example.api.ElpriserAPI;
 
+import javax.naming.CompositeName;
+
 public class Main {
+    private static CompositeName idagPriser;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,11 +20,11 @@ public class Main {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
 
         System.out.print("Skriv in zon (SE1, SE2, SE3 eller SE4): ");
-        String zone = scanner.nextLine();
+        String zone = scanner.nextLine().trim().toUpperCase();
 
-        while (!zone.equals("SE1")&&!zone.equals("SE2")&&!zone.equals("SE3")&&!zone.equals("SE4")) {
+        while (!zone.equals("SE1") && !zone.equals("SE2") && !zone.equals("SE3") && !zone.equals("SE4")) {
             System.out.print("Skriv in zon (SE1, SE2, SE3 eller SE4): ");
-            zone = scanner.nextLine();
+            zone = scanner.nextLine().trim().toUpperCase();
         }
 
         System.out.println("Vald zon är: " + zone);
@@ -37,7 +45,57 @@ public class Main {
             default:
                 System.out.println("Okänd zon.");
         }
-        scanner.close();
+
+        ElpriserAPI.Prisklass prisklass = ElpriserAPI.Prisklass.valueOf(zone);
+        LocalDate idag = LocalDate.now();
+        List<ElpriserAPI.Elpris> priser = elpriserAPI.getPriser(idag, prisklass);
+        System.out.println("Antal priser hämtade: " + priser.size());
+
+        int gräns = Math.min(3, priser.size());
+        if (priser.isEmpty()) {
+            System.out.println("Det finns inga priser för " + idag + ".");
+        } else {
+            for (int i = 0; i < gräns; i++) {
+            ElpriserAPI.Elpris pris = priser.get(i);
+            System.out.printf("Tid: %s Pris: %.2f SEK/kWh.%n", pris.timeStart().toLocalTime(), pris.sekPerKWh());
+            }
+        }
+
+        double summa = 0.0;
+        int antal = 0;
+
+        for (ElpriserAPI.Elpris pris : priser) {
+            boolean ärIdag = pris.timeStart().toLocalDate().equals(idag);
+            if (ärIdag) {
+                summa += pris.sekPerKWh();
+                antal++;
+            }
+        }
+
+        if (antal == 0) {
+            System.out.println("Det finns inga värden.");
+        } else {
+            double medelvärdeAvPriser = summa / antal;
+            System.out.printf("Medelpriset för %s under %dh är %.2f sek/kWh.%n", idag, antal, medelvärdeAvPriser);
+        }
+
+        if (idagPriser.isEmpty()) {
+            System.out.println("Det finns inga priser för dagens datum.");
+        } else {
+            ElpriserAPI.Elpris max = idagPriser.get(0);
+
+            for (int i = 1; i < idagPriser.size(); i++) {
+                ElpriserAPI.Elpris pris = idagPriser.get(i);
+                if (pris.sekPerKWh() < min.sekPerKWh()) {
+                    min = pris;
+                }
+                if (pris.sekPerKWh() > max.sekPerKWh()) {
+                    max = pris;
+                }
+            }
+            System.out.printf("Billigast: %s %.2f sek/kWh.%n", min.timeStart().toLocalTime(), min.sekPerKWh());
+            System.out.printf("Dyrast: %s %.2f sek/kWh.%n", max.timeStart().toLocalTime(), max.sekPerKWh());
+        }
     }
 }
-// if zone inte är se123 visa felmeddelande
+    // lägg in scanner.close();
